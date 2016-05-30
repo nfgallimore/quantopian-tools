3fe1fd573c28dfa63db83b66ce7561c5570ac8e7
Derek Miller <20miller10@gmail.com>
2016-05-30 16:27:19 -0700

    Fixing docs build.

--- python_template/__init__.py
+++ python_template/__init__.py
@@ -4,14 +4,25 @@ Python library template project.
 """
 from __future__ import print_function, absolute_import, division, unicode_literals
 
+#: Package Name
 __pkg_name__ = 'python_template'
+#: Version
 __version__ = '0.0.1'  # Follow semantic versioning (http://semver.org/)
+#: Release Date
 __release_date__ = '05/30/2016'
+#: License
 __license__ = 'BSD'
+#: Project Name
 __project_name__ = 'Python library Template'
+#: Project Description
 __project_description__ = __doc__.strip()
+#: Project URL
 __project_url__ = 'https://github.com/derek-miller/python-template'
+#: Author
 __author__ = 'Derek Miller'
+#: Author Email
 __author_email__ = 'derek@dmiller.me'
+#: Maintainer
 __maintainer__ = 'Derek Miller'
+#: Maintainer Email
 __maintainer_email__ = 'derek@dmiller.me'
